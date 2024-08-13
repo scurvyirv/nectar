dba1cb328b392610dd0cef1e1e6cea74e8bea394
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-08-12 19:04:23 -0700

    feat: updated userRoutes to include all CRUD operations, insomnia seems to get validation errors for POST

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,6 +10,12 @@ const {
 // /api/Users (create new user)
 router.route("/").get(getUsers).post(createUser);
 
+// /api/Users (view all users)
+router.route("/").get(getUsers);
+
+// /api/Users/:UserId (get single user)
+router.route("/:UserId").get(getSingleUser);
+
 // /api/Users/:UserId (update existing user)
 router.route("/:UserId").get(getSingleUser).put(updateUser);
 
